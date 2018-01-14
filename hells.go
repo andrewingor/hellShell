8f4b0fea0e3e6348d2438db3de86b572ee907c50
andrewingor <andrewingor@gmail.com>
2018-01-14 16:11:59 +0200

    Release v1.0

--- hells.go
+++ hells.go
@@ -3,10 +3,7 @@
 /*
 HellShell is RA tools
 It is simple http server for run remote shell with file transfer option
-Run hells and open at browser port 1666:
-
-http://localhost:1666/	File Server
-http://localhost:1666/s	Form Shell
+Run hells and open at browser port 1666: http://localhost:1666/
 
 UNDER CONSTRUCT
 
@@ -18,6 +15,7 @@ Usage
 package main
 
 import (
+	"os"
 	"io"
 	"fmt"
 	"html"
@@ -27,35 +25,54 @@ import (
 //Data
 const Revision = "$Id$" // Revision ID
 var (
-	echo []byte //Output of command
+	stdout []byte //Output of command
 	err  error  //Error
 )
-//Conclusion The Contract (HTTP response) 
-func contract ( response http.ResponseWriter, request *http.Request) {
-	io.WriteString(response, webmuzzle)
+//myContract (HTTP response) 
+func myContract (serv http.Handler ) http.Handler {
+	return http.HandlerFunc ( func (w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, htmlhead)//Before
+//TODO<a href="/"> Navigation / Here /  </a><br/>
+		serv.ServeHTTP(w, r)//Call origin
+//TODO<a href="/"> Navigation / Here /  </a><br/>
+		io.WriteString(w, htmlform)//Append
 
-	echo, err = exec.Command("cmd", "/C", request.FormValue("cmd")).Output()
-	if err != nil {
-		fmt.Println(err)
-		io.WriteString(response, err.Error())
-	}
-	io.WriteString(response, html.EscapeString( string(echo)) )
+		if r.Method == "POST" { //Upload file
+//TODO Uplad file to current directory in URL path
+           r.ParseMultipartForm(32 << 20)
+           if file, handler, err := r.FormFile("uploadfile"); err == nil {
+	          defer file.Close()
+ 	         	 f, err := os.OpenFile("./"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+ 	   			if err != nil {
+             	  fmt.Println(err)
+	           }
+ 	  	        defer f.Close()
+ 	         io.Copy(f, file)
+		   } else {
+			   fmt.Println(err)
+		   }
+       }
 
-	io.WriteString(response, "</pre><hr>"+request.FormValue("cmd"))
-	io.WriteString(response, "</body></html>")
+		if len (r.URL.Query().Get("cmd")) > 0 { //Run shell command
+			stdout, err = exec.Command("cmd", "/C", r.FormValue("cmd")).Output()
+			if err != nil { io.WriteString(w, err.Error())
+		}
+		io.WriteString(w, html.EscapeString( string(stdout)) )
+		}
+		io.WriteString(w, htmltail)//After
+	})
 }
-//
+//HellShell init
 func init() {
-	http.HandleFunc("/s", contract)
-	http.Handle("/", http.FileServer(http.Dir("/")))
+	http.Handle("/", myContract( http.FileServer(http.Dir("/"))) )
 	http.ListenAndServe(":1666", nil)
 }
-//------------------------------
-//HellShell
-func main() {
-}
+//HellShell run
+func main() {}
 //Web-muzzle
-var webmuzzle string = `
+var htmltail string = "</pre></body></html>"
+
+var htmlhead string = `
 <!DOCTYPE html>
 <html>
 <head><title>Hell$hell</title></head>
@@ -85,12 +102,19 @@ input {
 }
 </style>
 <body>
-<a href="/">localhost:1666/ File Server</a><br/>
-<a href="/s">localhost:1666/s Shell</a>
+`;
+
+var htmlform string = `
 <div class="middle">
 	<form id="cmdstr">
 cmd.exe&gt;<input class="cmd" type="text" name="cmd" value="" autofocus />
 		<input type="submit" value="Enter" /><br/>
+	</form>
+	<hr/>
+	<form enctype="multipart/form-data" action="" method="post">
+ 		<input type="file" name="uploadfile" />
+ 	   <input type="hidden" name="token" value="{{.}}"/>
+ 	  	 <input type="submit" value="upload" />
 	</form>
 </div>
 <script type="text/javascript">document.cmdstr.cmd.focus();</script>
